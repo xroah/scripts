0bd58716ffc7e851fa6b305d246a72216dacb4b3
xroah <396069583@qq.com>
2020-04-09 21:44:15 +0800

    add production configuration

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -3,7 +3,7 @@ const MiniCSSPlugin = require("mini-css-extract-plugin");
 const TerserPlugin = require("terser-webpack-plugin");
 const OptimizeCSSPlugin = require("optimize-css-assets-webpack-plugin");
 const fs = require("fs");
-const base = require("./baseConf");
+const getBaseConf = require("./baseConf");
 
 const optimization = {
     minimizer: [
@@ -20,5 +20,25 @@ const optimization = {
 }
 
 module.exports = env => {
+    const isProd = env === "production";
+    const useTypescript = fs.existsSync("../tsconfig.json");
+    const base = getBaseConf(useTypescript);
     process.env.NODE_ENV = env;
+    base.module.rules.push({
+        test: /\.css$/,
+        use: [
+            isProd ? MiniCSSPlugin.loader : "style-loader",
+            "css-loader"
+        ]
+    });
+
+    if (isProd) {
+        base.plugins.push(
+            new MiniCSSPlugin({
+                filename: "index.css",
+                chunkFilename: "chunk.[name].[id].css"
+            })
+        );
+        base.optimization = optimization;
+    }
 }
