d7f85e0671a5e269ac5187bef75f8279ca87d9c0
xroah <396069583@qq.com>
2020-04-12 14:58:30 +0800

    fix import css error

--- template/ts/src/App.tsx
+++ template/ts/src/App.tsx
@@ -1,7 +1,7 @@
 import { hot } from "react-hot-loader/root";
 import React from "react";
 import logo from "../public/logo.svg";
-import "./app.css";
+import "./App.css";
 
 class App extends React.Component {
     state = {};
