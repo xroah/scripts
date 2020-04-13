eb8b1f99688af0d4a2a0d2555417b379a71fca4e
xroah <396069583@qq.com>
2020-04-13 14:01:23 +0800

    print file sizes info after building

--- build/build.js
+++ build/build.js
@@ -3,12 +3,79 @@ const clean = require("./utils/clean");
 const webpack = require("webpack");
 const chalk = require("chalk");
 const ora = require("ora");
-const spinner = ora("building");
-
+const spinner = ora("Building");
+const path = require("path");
 const prodConf = conf("production");
+const outputPath = prodConf.output.path || path.join(process.cwd(), "dist");
+
+function resizeString(str, length) {
+    const l = str.length;
+
+    if (l >= length) {
+        return str.substring(l, length + 1);
+    }
+
+    const restLen = length - l;
+
+    return str + Array(restLen).fill(" ").join("");
+}
+
+function convertSize(size) {
+    const BASE = 1024;
+    const sizeKB = size / BASE;
+
+    //Byte
+    if (size < BASE) {
+        return `${size}B`;
+    }
+
+    //KB
+    if (sizeKB < BASE) {
+        return `${Math.ceil(sizeKB)}KB`;
+    }
+
+    //MB
+    return `${Math.ceil(sizeKB / Base)}MB`;
+}
+
+function statsAssets(assets)  {
+    const js = [];
+    const css = [];
+
+    assets.forEach(asset => {
+        const {
+            name,
+            size
+        } = asset;
+        const baseDir = path.resolve(__dirname, "..");
+        const ext = path.extname(name);
+        const filePath = outputPath + path.sep + name;
+        const relativePath = path.relative(baseDir, filePath);
+        const sizeStr = resizeString(convertSize(size), 10);
+        const ret = "  " + sizeStr + path.normalize(relativePath);
+
+        switch (ext) {
+            case ".js":
+                js.push(ret);
+                break;
+            case ".css":
+                css.push(ret);
+                break;
+        }
+    });
+
+    console.log("File sizes: ");
+    console.log();
+
+    js.concat(css).forEach(str => {
+        console.log(chalk.cyan(str));
+    });
+
+    console.log();
+}
 
 try {
-    clean(prodConf.output.path);
+    clean(outputPath);
 } catch (error) {
     console.log(error);
 }
@@ -26,6 +93,8 @@ webpack(prodConf, (err, stats) => {
 
     spinner.stop();
 
-    console.log();
     console.log(chalk.green("Built successfully"));
+    console.log();
+
+    statsAssets(stats.toJson().assets);
 });
