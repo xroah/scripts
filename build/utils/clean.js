10a8416afdee0d955a3c8f335ea28795b09b34ea
xroah <396069583@qq.com>
2020-04-18 09:49:07 +0800

    use lstat

--- build/utils/clean.js
+++ build/utils/clean.js
@@ -3,40 +3,7 @@ const path = require("path");
 
 module.exports = function clean(dir) {
     const absPath = path.resolve(dir);
-    let stat;
-    /**
-     * readdirSync may read the files,
-     * but statSync some files may cause error(does not exist),
-     * however, the file can be deleted:
-     * 
-     * > fs.readdirSync("./node_modules/.bin")
-        [ 'json5', 'loose-envify' ]
-        > fs.statSync("./node_modules/.bin/json5")
-        Uncaught Error: ENOENT: no such file or directory, stat './node_modules/.bin/json5'
-            at Object.statSync (fs.js:982:3) {
-        errno: -2,
-        syscall: 'stat',
-        code: 'ENOENT',
-        path: './node_modules/.bin/json5'
-        }
-        > fs.unlink("./node_modules/.bin/json5")
-     */
-
-    try {
-        stat = fs.statSync(absPath);
-    } catch (error) {
-
-    }
-
-    if (!stat) {
-        try {
-            fs.unlinkSync(absPath);
-        } catch (error) {
-            
-        }
-
-        return;
-    }
+    let stat = fs.lstatSync(absPath);
 
     if (stat.isDirectory(absPath)) {
         const files = fs.readdirSync(absPath);
