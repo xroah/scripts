f9e8bfbdfe4446855fca8a380d035c5ebacb1424
xroah <396069583@qq.com>
2020-04-19 20:39:41 +0800

    remove retries of rmdirSync and use path.join

--- build/utils/clean.js
+++ build/utils/clean.js
@@ -2,24 +2,20 @@ const fs = require("fs");
 const path = require("path");
 
 module.exports = function clean(dir) {
-    const absPath = path.resolve(dir);
-    let stat = fs.lstatSync(absPath);
+    let stat = fs.lstatSync(dir);
 
-    if (stat.isDirectory(absPath)) {
-        const files = fs.readdirSync(absPath);
+    if (stat.isDirectory(dir)) {
+        const files = fs.readdirSync(dir);
 
         while (files.length) {
             const file = files.pop();
-            const filePath = path.resolve(absPath, file);
+            const filePath = path.resolve(dir, file);
 
             clean(filePath);
         }
 
-        fs.rmdirSync(absPath, {
-            maxRetries: 5,
-            retryDelay: 200
-        });
+        fs.rmdirSync(dir);
     } else {
-        fs.unlinkSync(absPath);
+        fs.unlinkSync(dir);
     }
 }
