6f4995a35d51db7d97fa777420a18693e3d230ea
xroah <396069583@qq.com>
2020-12-31 13:06:50 +0800

    build command

--- src/bin/commands/build.ts
+++ src/bin/commands/build.ts
@@ -1,11 +1,114 @@
 import {program} from "commander"
+import merge from "../utils/merge"
+import {build as webpackBuild} from "webpack-build-helper"
+import prodConf from "../../config/webpack/webpack.prod"
+import path from "path"
+import getRollupOptions from "../../config/rollup/rollup.config"
+import {rollup} from "rollup"
+import ora from "ora"
+import chalk from "chalk"
+import rimraf from "rimraf"
+
+interface RollupBuildOptions {
+    entry?: string
+    libName?: string
+    outDir?: string
+    ts?: boolean
+    config?: string,
+    include?: string
+}
 
 const build = program.command("build")
 
+async function rollupBuild(buildOptions: RollupBuildOptions) {
+    const {
+        ts,
+        config
+    } = buildOptions
+    const cmdSet = new Set([
+        "entry",
+        "outDir",
+        "libName",
+        "include"
+    ])
+    const loading = ora("Building for production use rollup")
+    const cmdConfig: any = {}
+    let mergedConfig: any = {}
+
+    loading.start()
+
+    if (config) {
+        mergedConfig = require(path.join(process.cwd(), config)).rollup
+    }
+
+    for (let c in buildOptions) {
+        if (cmdSet.has(c)) {
+            cmdConfig[c] = (buildOptions as any)[c]
+        }
+    }
+
+    mergedConfig = {
+        ...mergedConfig,
+        ...cmdConfig
+    }
+
+    const {
+        options: rollupOptions,
+        outputOption,
+        outputProdOption,
+        dist
+    } = getRollupOptions(mergedConfig, ts)
+
+    try {
+        rimraf.sync(dist)
+    } catch (error) {
+
+    }
+
+    try {
+        const bundle = await rollup(rollupOptions)
+
+        await bundle.write(outputOption)
+        await bundle.write(outputProdOption)
+        await bundle.close()
+
+        loading.stop()
+        console.log()
+        console.log(chalk.green("Built successfully"))
+        console.log()
+    } catch (error) {
+        console.log(error)
+        loading.stop()
+    }
+}
+
+function action(cmd: any) {
+    const {
+        rollup,
+        config,
+        ts,
+        entry
+    } = cmd
+
+    if (!rollup) {
+        const {merged} = merge(prodConf, config, ts)
+
+        if (entry) {
+            merged.output!.path = path.join(process.cwd(), entry)
+        }
+
+        return webpackBuild(merged)
+    }
+console.log("000000000000", cmd.include)
+    rollupBuild(cmd)
+}
+
 build
-    .option("-w, --webpack", "Use webpack to build")
     .option("-r, --rollup", "Use rollup to build")
+    .option("--no-ts", "Use javascript")
     .option("-c, --config <value>", "Configuration file")
-    .action(function (cmd) {
-        console.log(cmd.webpack, cmd.rollup, cmd.config, "<<<<<")
-    })
+    .option("-o, --outDir <value>", "Output dir")
+    .option("-e, --entry <value>", "Entry file")
+    .option("-n, --libName <value>", "Library name(rollup only)")
+    .option("-i, --include <value>", "Typescript includes(rollup only)")
+    .action(action)
--- src/config/rollup/rollup.config.ts
+++ src/config/rollup/rollup.config.ts
@@ -1,5 +1,67 @@
-import {RollupOptions} from "rollup"
+import {RollupOptions, OutputOptions, OutputPlugin} from "rollup"
+import path from "path"
+import {terser} from "rollup-plugin-terser"
+import typescript from "@rollup/plugin-typescript"
+import resolve from "@rollup/plugin-node-resolve"
+import cjs from "@rollup/plugin-commonjs"
+import babel from "@rollup/plugin-babel"
+import getBabelConf from "../babel/babel.config"
 
-const options: RollupOptions = {}
+export default (customOption: any = {}, ts = true) => {
+    const cwd = process.cwd()
+    const dist = `${path.join(cwd, customOption.outDir || "dist")}`
+    const name = customOption.libName || "reap"
+    const commonOutputConf: OutputOptions = {
+        name,
+        format: "umd",
+        globals: {
+            "react": "React",
+            "react-dom": "ReactDOM",
+            ...customOption.globals
+        }
+    }
+    const plugins = [
+        resolve(),
+        cjs(),
+        ts && typescript({
+            tsconfig: false,
+            target: "ESNext",
+            jsx: "react",
+            strict: true,
+            moduleResolution: "node",
+            include: [path.join(cwd, customOption.include || "src")],
+            exclude: ["node_modules"],
+            esModuleInterop: true,
+            experimentalDecorators: true
+        }),
+        babel({
+            exclude: /node_modules/,
+            extensions: [".ts", ".tsx", ".js", ".jsx"],
+            babelHelpers: "runtime",
+            ...getBabelConf()
+        })
+    ].filter(Boolean) as any;
+    const outputOption: OutputOptions = {
+        ...commonOutputConf,
+        file: path.join(dist, `${name}.js`)
+    }
+    const outputProdOption: OutputOptions = {
+        ...commonOutputConf,
+        file: path.join(dist, `${name}.min.js`),
+        sourcemap: true,
+        plugins: [terser()]
+    }
+    const options: RollupOptions = {
+        input: path.join(cwd, customOption.entry || "./src/index.ts"),
+        output: [outputOption, outputProdOption],
+        plugins,
+        external: ["react", "react-dom"].concat(customOption.external)
+    }
 
-export default options
+    return {
+        options,
+        outputOption,
+        outputProdOption,
+        dist
+    }
+}
