848e33f7aeb809da4c8cca4519435b41f8401875
xroah <396069583@qq.com>
2021-01-21 19:49:45 +0800

    utils

---
+++ src/lib/utils/get-ip.ts
@@ -0,0 +1,27 @@
+import os from "os"
+import defaultGateway from "default-gateway"
+
+export default () => {
+    const ni = os.networkInterfaces()
+    const gateway = defaultGateway.v4.sync()
+
+    for (let item in ni) {
+        const tmp = ni[item]
+
+        for (let i = 0, l = tmp.length; i < l; i++) {
+            const {
+                address,
+                internal,
+                family
+            } = tmp[i]
+
+            if (
+                !internal &&
+                item === gateway.interface &&
+                family.toLowerCase() === "ipv4"
+            ) {
+                return address
+            }
+        }
+    }
+}
