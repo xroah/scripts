4171e5209864e8a22238c2be4d5214f9de2c40f1
xroah <396069583@qq.com>
2021-02-02 15:41:59 +0800

    revise jest configuration

--- src/config/jest/jest.config.ts
+++ src/config/jest/jest.config.ts
@@ -1,15 +1,20 @@
-import path from "path"
+import {resolve, join} from "path"
 
 export default {
     clearMocks: true,
-    coverageDirectory: path.join(process.cwd(), "coverage"),
+    coverageDirectory: join(process.cwd(), "coverage"),
     coverageProvider: "babel",
     testMatch: [
         "<rootDir>/tests/**/*.spec.[jt]s?(x)"
     ],
     testEnvironment: "jsdom",
     transform: {
-        "\\.[jt]sx?$": path.resolve(__dirname, "./babel-transformer.js")
+        "\\.[jt]sx?$": resolve(__dirname, "./babel-transform.js"),
+        "\\.s?css$": resolve(__dirname, ".css-transform.js"),
+        "^(?!.*\\.(js|jsx|mjs|cjs|ts|tsx|css|json)$)": resolve(__dirname, "./file-transform.js")
     },
-    transformIgnorePatterns: ["node_modules"]
+    transformIgnorePatterns: [
+        "[/\\\\]node_modules[/\\\\].+\\.(js|jsx|mjs|cjs|ts|tsx)$",
+        "\\.module\\.(css|sass|scss)$"
+    ]
 }
