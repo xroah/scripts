8ce6db61577ea5bbd18115a5a61929e73428f202
xroah <396069583@qq.com>
2021-02-03 16:44:12 +0800

    support configuration in package.json

--- src/bin/utils/load-config.ts
+++ src/bin/utils/load-config.ts
@@ -3,13 +3,17 @@ import fs from "fs"
 import getAbsPath from "./get-abs-path"
 
 export default (configFile?: string) => {
-    const defaultConfFile = path.join(process.cwd(), "reap.config.js")
+    const cwd = process.cwd()
+    const defaultConfFile = path.join(cwd, "reap.config.js")
+    const pkgJSON = path.join(cwd, "package.json")
     let config: any = {}
 
     if (configFile) {
         config = require(getAbsPath(configFile))
     } else if (fs.existsSync(defaultConfFile)) {
         config = require(defaultConfFile)
+    } else if (fs.existsSync(pkgJSON)) {
+        config = require(pkgJSON).reap || {}
     }
 
     return config
