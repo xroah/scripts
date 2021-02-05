2b44b9692efd836c0e800f23015e4281c2c62a55
xroah <396069583@qq.com>
2021-02-05 09:11:15 +0800

    add NODE_ENV

--- template/build/build.js
+++ template/build/build.js
@@ -2,6 +2,8 @@ const prodConf = require("./config/prod.config")
 const builderHelper = require("webpack-build-helper")
 const rimraf = require("rimraf")
 
+process.env.NODE_ENV = "production"
+
 try {
     rimraf.sync(prodConf.output.path)
 } catch (error) {
--- template/build/start.js
+++ template/build/start.js
@@ -2,4 +2,6 @@ const devConf = require("./config/dev.config")
 const serverConf = require("./config/server.config")
 const builderHelper = require("webpack-build-helper")
 
+process.env.NODE_ENV = "development"
+
 builderHelper.start(devConf, serverConf)
