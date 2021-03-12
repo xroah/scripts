68eb9eb0f59b2e95e894ee94c936b9a9610e7f44
xroah <396069583@qq.com>
2021-03-12 16:45:57 +0800

    bug fix

--- src/config/jest/jest.config.ts
+++ src/config/jest/jest.config.ts
@@ -10,7 +10,7 @@ export default {
     testEnvironment: "jsdom",
     transform: {
         "\\.[jt]sx?$": resolve(__dirname, "./babel-transform.js"),
-        "\\.s?css$": resolve(__dirname, ".css-transform.js"),
+        "\\.s?css$": resolve(__dirname, "./css-transform.js"),
         "^(?!.*\\.(js|jsx|mjs|cjs|ts|tsx|css|json)$)": resolve(__dirname, "./file-transform.js")
     },
     transformIgnorePatterns: [
