2d4ebd5142d8174666b4aac8177882e6743dc429
xroah <396069583@qq.com>
2021-04-01 12:21:16 +0800

    local vairable

--- src/utils/transpile-ts.ts
+++ src/utils/transpile-ts.ts
@@ -4,27 +4,26 @@ import path from "path"
 import getAbsPath from "./get-abs-path";
 import getProjectRoot from "./get-project-root";
 
-const cacheDir = path.join(getProjectRoot(), ".cache")
-
-if (!fs.existsSync(cacheDir)) {
-    fs.mkdirSync(cacheDir)
-}
-
 export default (filename: string) => {
+    const cacheDir = path.join(getProjectRoot(), ".cache")
     const source = fs.readFileSync(getAbsPath(filename)).toString()
     const {name} = path.parse(filename)
     const outputFilename = path.join(cacheDir, `${name}.js`)
     const result = transpileModule(source, {compilerOptions: {module: ModuleKind.CommonJS}});
 
+    if (!fs.existsSync(cacheDir)) {
+        fs.mkdirSync(cacheDir)
+    }
+
     fs.writeFileSync(outputFilename, result.outputText.toString())
 
     process.nextTick(() => {
         try {
-           fs.unlinkSync(outputFilename) 
+            fs.unlinkSync(outputFilename)
         } catch (error) {
             // do nothing
         }
     })
-    
+
     return outputFilename
 }
