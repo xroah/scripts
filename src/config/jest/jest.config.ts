673913c1884092603772931620c5e598fa1107bd
xroah <396069583@qq.com>
2021-04-03 13:00:03 +0800

    misc

--- src/config/jest/jest.config.ts
+++ src/config/jest/jest.config.ts
@@ -11,9 +11,5 @@ export default {
         "\\.[jt]sx?$": resolve(__dirname, "./babel-transform.js"),
         "\\.s?css$": resolve(__dirname, "./css-transform.js"),
         "^(?!.*\\.(js|jsx|mjs|cjs|ts|tsx|css|json)$)": resolve(__dirname, "./file-transform.js")
-    },
-    transformIgnorePatterns: [
-        "[/\\\\]node_modules[/\\\\].+\\.(js|jsx|mjs|cjs|ts|tsx)$",
-        "\\.module\\.(css|sass|scss)$"
-    ]
+    }
 }
