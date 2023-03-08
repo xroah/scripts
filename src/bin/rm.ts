68d6aa5f1998fd149ee6cc7729d2543ebab19a74
xroah <396069583@qq.com>
2023-03-08 13:09:30 +0800

    verbose option

--- src/bin/rm.ts
+++ src/bin/rm.ts
@@ -7,15 +7,27 @@ export default function createRMCommand(y: typeof yargs) {
     return y.command(
         "rm <files...>",
         "Remove the files or directories",
-        {},
+        {
+            verbose: {
+                type: "boolean",
+                desc: "Show details"
+            }
+        },
         argv => {
             const files = argv.files as string[]
+            const filter = (f: string) => {
+                if (argv.verbose) {
+                    console.log(f)
+                }
+
+                return true
+            }
 
             for (const f of files) {
                 const absFilePath = getAbsPath(f)
 
                 if (fs.existsSync(absFilePath)) {
-                    rimraf.sync(absFilePath)
+                    rimraf.sync(absFilePath, { filter })
                 }
             }
         }
